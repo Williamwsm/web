c5d61054fd307670baceb6b8a9c8fb48baf5bd77
William Santos <113859999+Williamwsm@users.noreply.github.com>
2024-05-13 11:12:16 -0300

    commit
    adicao da biblioteca rome e um pequeno teste, analisando e imprimindo o feed rss do g1

---
+++ rss_g1/src/main/java/com/example/rss_g1/rome/Rome.java
@@ -0,0 +1,39 @@
+package com.example.rss_g1.rome;
+
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+
+import java.net.URL;
+
+public class Rome {
+    private String urlFeed;
+
+    public Rome(String urlFeed) {
+        this.urlFeed = urlFeed;
+    }
+
+    public void lerFeed() {
+        try {
+            URL url = new URL(urlFeed);
+
+            SyndFeedInput entrada = new SyndFeedInput();
+            SyndFeed feed = entrada.build(new XmlReader(url));
+
+            System.out.println("Título do Feed: " + feed.getTitle());
+            System.out.println("Link do Feed: " + feed.getLink());
+            System.out.println("Descrição do Feed: " + feed.getDescription());
+
+            feed.getEntries().forEach(noticia -> {
+                System.out.println("Título da Notícia: " + noticia.getTitle());
+                System.out.println("Link da Notícia: " + noticia.getLink());
+                System.out.println("Descrição da Notícia: " + noticia.getDescription().getValue());
+                System.out.println("Data de Publicação: " + noticia.getPublishedDate());
+                System.out.println("----------------------------------------");
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+}
