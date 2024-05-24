15482ba8bb0f3df6f503d64da2da872563f7f7f2
William Santos <113859999+Williamwsm@users.noreply.github.com>
2024-05-24 14:38:06 -0300

    Update RssConsumidorService.java
    add as aconotacoes @EnableScheduling
    @Component

--- rss_g1/src/main/java/com/example/rss_g1/service/RssConsumidorService.java
+++ rss_g1/src/main/java/com/example/rss_g1/service/RssConsumidorService.java
@@ -7,7 +7,9 @@ import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -19,6 +21,8 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 @Service
+@EnableScheduling
+@Component
 public class RssConsumidorService {
     @Autowired
     private NoticiaRepository noticiaRepository;
